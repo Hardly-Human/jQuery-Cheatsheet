5aa462183d2cf728f6a1f6254b031b4168cbdc36
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-07-13 21:51:50 +0530

    Updated README.md

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 // jQuery..
 
 $(document).ready(function () {
-	// // add class to HTML Element.
-	// $("button").addClass("text-red");
+	// add class to HTML Element.
+	$("button").addClass("text-red");
 
-	// // add class to element selected by class.
-	// $(".well").addClass("animate__animated animate__swing");
+	// add class to element selected by class.
+	$(".well").addClass("animate__animated animate__swing");
 
-	// // add class to element selected by id.
-	// $("#target1").addClass("animate__animated animate__bounce");
+	// add class to element selected by id.
+	$("#target1").addClass("animate__animated animate__bounce");
 
-	// // change css of element
-	// $("#target3").css("color", "white");
+	// change css of element
+	$("#target3").css("color", "white");
 
-	// // change HTML attributes
-	// $("#target5").prop("disabled", true);
+	// change HTML attributes
+	$("#target5").prop("disabled", true);
 
-	// // change text inside HTML elements
-	// $("#target6").html("<em>target6</em>");
+	// change text inside HTML elements
+	$("#target6").html("<em>target6</em>");
 
-	// //remove Element
-	// $("#target4").remove();
+	//remove Element
+	$("#target4").remove();
 
-	// // append elements to other
-	// $("#target2").appendTo("#right-well");
+	// append elements to other
+	$("#target2").appendTo("#right-well");
 
-	// // Clone Element
-	// $("#target2").clone().appendTo("#left-well");
+	// Clone Element
+	$("#target2").clone().appendTo("#left-well");
 
-	// // target parent element
-	// $("#target1").parent().css("background-color", "black");
+	// target parent element
+	$("#target1").parent().css("background-color", "black");
 
-	// // target Children
-	// $("#right-well").children().addClass("animate__animated animate__jello");
+	// target Children
+	$("#right-well").children().addClass("animate__animated animate__jello");
 
-	// // target specific
-	// $(".target:nth-child(2)").addClass("animate__animated animate__bounce");
+	// target specific
+	$(".target:nth-child(2)").addClass("animate__animated animate__bounce");
 
 	// target even/odd elements
 	$(".target:even").addClass("animate__animated animate__bounce");
